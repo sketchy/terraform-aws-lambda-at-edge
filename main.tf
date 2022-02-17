e305f219d354eb92524ba5b3d4a99534aea2179c
Joshua Nisenson <josh@sketchy.com>
2022-02-17 15:16:34 -0800

    Trying to create a dependency chain

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
  */
 data "archive_file" "zip_file_for_lambda" {
   type        = "zip"
-  output_path = "${var.local_file_dir}/${var.name}.zip"
+  output_path = "${path.module}/${var.local_file_dir}/${var.name}.zip"
 
   dynamic "source" {
     for_each = distinct(flatten([
@@ -22,8 +22,8 @@ data "archive_file" "zip_file_for_lambda" {
     ]))
     content {
       content = try(
-        file("${var.lambda_code_source_dir}/${source.value}"),
-        filebase64("${var.lambda_code_source_dir}/${source.value}"),
+        file("${path.module}/../${var.lambda_code_source_dir}/${source.value}"),
+        filebase64("${path.module}/../${var.lambda_code_source_dir}/${source.value}"),
       )
       filename = source.value
     }
